0136eba3876b582eaacf08cc3e8c5e4fc396b378
Max <rudko.max10@gmail.com>
2021-04-19 00:47:00 +0300

    Create basic UI

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
+#include <arrays/Collection.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    std::vector<Collection *> collections;
 };
 #endif // MAINWINDOW_H
